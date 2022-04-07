873c57f8f0c3c2b9c3647fa63253323ff417e15d
ISRAEL_TIENTCHEU <tientcheuisrael@gmail.com>
2022-04-08 00:00:03 +0100

    Ajout d'un fragment ( fragment_home.xml ) dans le main page et lancement de l'application sur le telephone android pour la premiere fois

--- app/src/main/java/com/example/naturecollection/MainActivity.kt
+++ app/src/main/java/com/example/naturecollection/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.naturecollection.fragments.home_fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //injection du fragment dans notre boite(fragment_container)
+        val transaction=supportFragmentManager.beginTransaction()//manipulation de la dynamique des fragment
+        transaction.replace(R.id.fragment_container,home_fragment())//remplacement du contenu du fragment
+        transaction.addToBackStack(null)//pas avoir de retour sur ce composant
+        transaction.commit()//Envoyer l'echangement
     }
 }
