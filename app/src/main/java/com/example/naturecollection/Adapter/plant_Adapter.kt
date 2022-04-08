cddc50c9b741b0c4704a533b0e5ac6c6cce4ee58
ISRAEL_TIENTCHEU <tientcheuisrael@gmail.com>
2022-04-08 14:50:54 +0100

    Finalisation de l'insertion de l'adapter dans le fragment

--- app/src/main/java/com/example/naturecollection/Adapter/plant_Adapter.kt
+++ app/src/main/java/com/example/naturecollection/Adapter/plant_Adapter.kt
@@ -8,23 +8,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.naturecollection.R
 
 class plant_Adapter:RecyclerView.Adapter<plant_Adapter.ViewHolder>() {
-    //Pour porter la vue (boite pour ranger tout les composant a controler)
+
+    //Pour porter la vue (boite pour ranger tout les composant a controller)
+
     class ViewHolder(view:View):RecyclerView.ViewHolder(view){
         //importer la plante
         val plantImage=view.findViewById<ImageView>(R.id.image_item)
     }
 
+    //Injecter un layout
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view= LayoutInflater
             .from(parent.context)
             .inflate(R.layout.item_horizontal_plant,parent,false)
+
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {}
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = 5
 }
--- app/src/main/java/com/example/naturecollection/fragments/home_fragment.kt
+++ app/src/main/java/com/example/naturecollection/fragments/home_fragment.kt
@@ -5,11 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.naturecollection.Adapter.plant_Adapter
 import com.example.naturecollection.R
 
 class home_fragment: Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_home,container,false)
+        val view= inflater?.inflate(R.layout.fragment_home,container,false)
+
+        //recuperation du recycle view
+        val horizontalRecycleView=view.findViewById<RecyclerView>(R.id.horizontal_recylcleView)
+        horizontalRecycleView.adapter= plant_Adapter()
+        return view
     }
 }
